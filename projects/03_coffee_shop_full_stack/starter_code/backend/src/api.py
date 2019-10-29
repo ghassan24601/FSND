594f8740d78bc39f062cf974405cbfde3d87a3cc
Ghassan Al Mamar <Ghassan.Al.Mamar@oracle.com>
2019-10-28 19:31:27 -0700

    Second shot

--- projects/03_coffee_shop_full_stack/starter_code/backend/src/api.py
+++ projects/03_coffee_shop_full_stack/starter_code/backend/src/api.py
@@ -12,93 +12,74 @@ setup_db(app)
 CORS(app)
 
 
-'''
+"""
 @TODO uncomment the following line to initialize the datbase
 !! NOTE THIS WILL DROP ALL RECORDS AND START YOUR DB FROM SCRATCH
 !! NOTE THIS MUST BE UNCOMMENTED ON FIRST RUN
-'''
+"""
 db_drop_and_create_all()
 
-d = Drink(title='Old Fashioned',
-        recipe='Mix a bunch of stuff together')
+d = Drink(title="Old Fashioned", recipe="Mix a bunch of stuff together")
 
 d.insert()
 
 
 ## ROUTES
 
+
 @app.route("/drinks", methods=["GET", "POST"])
-@requires_auth(permission='post:drinks')
+@requires_auth(permission="post:drinks")
 def drinks():
-    if request.method == 'GET':
+    if request.method == "GET":
         drinks = Drink.query.all()
-        return jsonify({
-            "success": True,
-            "drinks": [d.short() for d in drinks]
-        }), 200
+        return jsonify({"success": True, "drinks": [d.short() for d in drinks]}), 200
     else:
-        drink = Drink(title=request.args.get('title'),
-              recipe=request.args.get('recipe'))
+        drink = Drink(
+            title=request.args.get("title"), recipe=request.args.get("recipe")
+        )
         drink.insert()
-        return jsonify({
-            "success": True,
-            "drinks": drink.short()
-        })
+        return jsonify({"success": True, "drinks": drink.short()})
 
 
 @app.route("/drinks-detail")
-@requires_auth(permission='get:drinks-detail')
+@requires_auth(permission="get:drinks-detail")
 def drinks_detail():
     drinks = Drink.query.all()
-    return jsonify({
-        "success": True,
-        "drinks": [d.long () for d in drinks]
-    }), 200
+    return jsonify({"success": True, "drinks": [d.long() for d in drinks]}), 200
 
 
-@app.route("/drinks/<int:drink_id")
+@app.route("/drinks/<int:drink_id>")
 @requires_auth(permission="patch:drinks")
 def update_drink(drink_id):
     drink = Drink.query.get(drink_id).first()
-    drink.title = request.args.get('title')
-    drink.recipe = request.args.get('recipe')
+    drink.title = request.args.get("title")
+    drink.recipe = request.args.get("recipe")
     drink.update()
-    return jsonify({
-        "success": True,
-        "drinks": [d.long for d in drink]
-    }), 200
+    return jsonify({"success": True, "drinks": [d.long for d in drink]}), 200
+
 
 @app.route("/drinks/<int:drink_id")
 @requires_auth(permission="delete:drinks")
 def update_drink(drink_id):
     try:
         drink = Drink.query.get(drink_id).first()
-    except:
+    except exc.DBAPIError:
         abort(404)
     drink.delete()
-    return jsonify({
-        "success": True,
-        "delete": drink_id
-    }), 200
+    return jsonify({"success": True, "delete": drink_id}), 200
 
 
 ## Error Handling
-'''
+"""
 Example error handling for unprocessable entity
-'''
+"""
+
+
 @app.errorhandler(422)
 def unprocessable(error):
-    return jsonify({
-                    "success": False, 
-                    "error": 422,
-                    "message": "unprocessable"
-                    }), 422
+    return jsonify({"success": False, "error": 422, "message": "unprocessable"}), 422
+
 
 @app.errorhandler(404)
-def unprocessable(error):
-    return jsonify({
-                    "success": False,
-                    "error": 404,
-                    "message": "unprocessable"
-                    }), 404
-
+def entity_not_found(error):
+    return jsonify({"success": False, "error": 404, "message": "unprocessable"}), 404
--- projects/03_coffee_shop_full_stack/starter_code/backend/src/auth/auth.py
+++ projects/03_coffee_shop_full_stack/starter_code/backend/src/auth/auth.py
@@ -5,15 +5,17 @@ from jose import jwt
 from urllib.request import urlopen
 
 
-AUTH0_DOMAIN = 'ghassan.auth0.com'
-ALGORITHMS = ['RS256']
-API_AUDIENCE = 'dev'
+AUTH0_DOMAIN = "ghassan.auth0.com"
+ALGORITHMS = ["RS256"]
+API_AUDIENCE = "dev"
 
 ## AuthError Exception
-'''
+"""
 AuthError Exception
 A standardized way to communicate auth failure modes
-'''
+"""
+
+
 class AuthError(Exception):
     def __init__(self, error, status_code):
         self.error = error
@@ -22,47 +24,48 @@ class AuthError(Exception):
 
 ## Auth Header
 
+
 def get_token_auth_header():
-    if 'Authorization' not in request.headers:
+    if "Authorization" not in request.headers:
         abort(401)
-    auth_header = request.headers['Authorization']
+    auth_header = request.headers["Authorization"]
     header_parts = auth_header.split(" ")
     if len(header_parts) != 2:
         abort(401)
-    elif header_parts[0].lower() != 'bearer':
+    elif header_parts[0].lower() != "bearer":
         abort(401)
     return header_parts[1]
 
 
 def check_permissions(permission, payload):
-    if 'permissions' not in payload:
-                        raise AuthError({
-                            'code': 'invalid_claims',
-                            'description': 'Permissions not included in JWT.'
-                        }, 400)
+    if "permissions" not in payload:
+        raise AuthError(
+            {
+                "code": "invalid_claims",
+                "description": "Permissions not included in JWT.",
+            },
+            400,
+        )
 
-    if permission not in payload['permissions']:
-        raise AuthError({
-            'code': 'unauthorized',
-            'description': 'Permission not found.'
-        }, 403)
+    if permission not in payload["permissions"]:
+        raise AuthError(
+            {"code": "unauthorized", "description": "Permission not found."}, 403
+        )
     return True
 
 
-
 def verify_decode_jwt(token):
-    jsonurl = urlopen(f'https://{AUTH0_DOMAIN}/.well-known/jwks.json')
+    jsonurl = urlopen(f"https://{AUTH0_DOMAIN}/.well-known/jwks.json")
     jwks = json.loads(jsonurl.read())
     unverified_header = jwt.get_unverified_header(token)
     rsa_key = {}
-    if 'kid' not in unverified_header:
-        raise AuthError({
-            'code': 'invalid_header',
-            'description': 'Authorization malformed.'
-        }, 401)
+    if "kid" not in unverified_header:
+        raise AuthError(
+            {"code": "invalid_header", "description": "Authorization malformed."}, 401
+        )
 
 
-def requires_auth(permission=''):
+def requires_auth(permission=""):
     def requires_auth_decorator(f):
         @wraps(f)
         def wrapper(*args, **kwargs):
@@ -72,4 +75,5 @@ def requires_auth(permission=''):
             return f(payload, *args, **kwargs)
 
         return wrapper
+
     return requires_auth_decorator
