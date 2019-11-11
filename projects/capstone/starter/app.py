1666de17791b4014e5a7dbfed755db9784a62f08
Ghassan Al Mamar <Ghassan.Al.Mamar@oracle.com>
2019-11-10 20:03:35 -0800

    Starting modeling capstone

--- projects/capstone/starter/app.py
+++ projects/capstone/starter/app.py
@@ -3,10 +3,23 @@ from flask import Flask, request, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
+from .models import Movies, Actors
+
 def create_app(test_config=None):
   # create and configure the app
   app = Flask(__name__)
   CORS(app)
+
+  @app.route('/actors')
+  def actors():
+      actors = Actors.query.all()
+      return jsonify([actor.format() for actor in actors])
+
+  @app.route('/movies')
+  def movies():
+      movies = Movies.query.all()
+      return jsonify([movie.format() for movie in movies])
+
 
   return app
 
